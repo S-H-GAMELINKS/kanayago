9819fb78b6c0e3b950f1d039ef959face04722d4
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-06-29 15:00:53 +0900

    Add Mjollnir.parse method

--- ext/mjollnir/mjollnir.c
+++ ext/mjollnir/mjollnir.c
@@ -2,8 +2,15 @@
 
 VALUE rb_mMjollnir;
 
+static VALUE
+parse(VALUE source)
+{
+  return Qtrue;
+}
+
 RUBY_FUNC_EXPORTED void
 Init_mjollnir(void)
 {
   rb_mMjollnir = rb_define_module("Mjollnir");
+  rb_define_singleton_method(rb_mMjollnir, "parse", parse, 1);
 }
