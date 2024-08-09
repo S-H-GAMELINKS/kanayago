fac571e1d8a84bf861d6dc2743f398e8ae5cfd0e
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-08-09 22:35:20 +0900

    Parse Ruby code and return Hash

--- ext/mjollnir/mjollnir.c
+++ ext/mjollnir/mjollnir.c
@@ -1,18 +1,61 @@
 #include "mjollnir.h"
-#include <ruby.h>
+#include "internal/ruby_parser.h"
+#include "ruby/ruby.h"
+#include "rubyparser.h"
 
 VALUE rb_mMjollnir;
 
-static VALUE
-parse(VALUE source)
+VALUE
+ast_to_values(VALUE hash, const NODE *node, int indent)
 {
-  const rb_parser_config_t *config = rb_ruby_parser_config();
-  return Qtrue;
+    enum node_type type;
+
+    if (!node) {
+        return Qnil;
+    }
+
+    type = nd_type(node);
+
+    switch (type) {
+	case NODE_SCOPE:
+	  rb_hash_aset(hash, rb_str_new2("NODE_SCOPE"), ast_to_values(hash, RNODE_SCOPE(node)->nd_body, indent++));
+	  return hash;
+	case NODE_INTEGER: {
+	  VALUE result = rb_hash_new();
+	  rb_hash_aset(result, rb_str_new2("NODE_INTEGER"), rb_node_integer_literal_val(node));
+	  return result;
+	}
+	default:
+	  return Qfalse;
+    }
+}
+
+static VALUE
+parse(VALUE self, VALUE source)
+{   
+    struct ruby_parser *parser;
+    rb_parser_t *parser_params;
+
+    parser_params = rb_parser_params_new();
+    VALUE vparser = TypedData_Make_Struct(0, struct ruby_parser,
+                                         &ruby_parser_data_type, parser);
+    parser->parser_params = parser_params;
+
+
+    VALUE vast = rb_parser_compile_string(vparser, "main", source, 0);
+
+    rb_ast_t *ast;
+
+    TypedData_Get_Struct(vast, rb_ast_t, &ast_data_type, ast);
+
+    VALUE result = rb_hash_new();
+
+    return ast_to_values(result, ast->body.root, 0);
 }
 
 RUBY_FUNC_EXPORTED void
 Init_mjollnir(void)
 {
-  rb_mMjollnir = rb_define_module("Mjollnir");
-  rb_define_singleton_method(rb_mMjollnir, "parse", parse, 1);
+    rb_mMjollnir = rb_define_module("Mjollnir");
+    rb_define_singleton_method(rb_mMjollnir, "parse", parse, 1);
 }
--- ext/mjollnir/mjollnir.h
+++ ext/mjollnir/mjollnir.h
@@ -2,8 +2,13 @@
 #define MJOLLNIR_H 1
 
 #include "node.h"
+#include "internal/ruby_parser.h"
 
 rb_parser_t *rb_parser_params_new(void);
-const rb_parser_config_t *rb_ruby_parser_config(void);
+VALUE rb_parser_compile_string(VALUE, const char *, VALUE, int);
+VALUE rb_node_integer_literal_val(const NODE *);
+
+extern const rb_data_type_t ruby_parser_data_type;
+extern const rb_data_type_t ast_data_type;
 
 #endif /* MJOLLNIR_H */
