c67dcc816be3854f76e367f803c411614493ca17
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-08-13 13:58:51 +0900

    Add NODE_RATIONAL parse test

---
+++ test/mjollnir/parse_rational_test.rb
@@ -0,0 +1,324 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ParseRationalTest < Minitest::Test
+  def test_parse_rational
+    result = Mjollnir.parse('1/17r')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_INTEGER' => 1
+          },
+          'mid' => :/,
+          'args' => {
+            'NODE_LIST' => [
+              {
+                'NODE_RATIONAL' => 17
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_parse_rational_plus_opcall
+    result = Mjollnir.parse('1/17r + 1/17r')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_OPCALL' => {
+              'recv' => {
+                'NODE_INTEGER' => 1
+              },
+              'mid' => :/,
+              'args' => {
+                'NODE_LIST' => [
+                  {
+                    'NODE_RATIONAL' => 17
+                  }
+                ]
+              }
+            }
+          },
+          'mid' => :+,
+          'args' => {
+            'NODE_LIST' => [
+              {
+                'NODE_OPCALL' => {
+                  'recv' => {
+                    'NODE_INTEGER' => 1
+                  },
+                  'mid' => :/,
+                  'args' => {
+                    'NODE_LIST' => [
+                      {
+                        'NODE_RATIONAL' => 17
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_parse_rational_minus_opcall
+    result = Mjollnir.parse('1/17r - 1/17r')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_OPCALL' => {
+              'recv' => {
+                'NODE_INTEGER' => 1
+              },
+              'mid' => :/,
+              'args' => {
+                'NODE_LIST' => [
+                  {
+                    'NODE_RATIONAL' => 17
+                  }
+                ]
+              }
+            }
+          },
+          'mid' => :-,
+          'args' => {
+            'NODE_LIST' => [
+              {
+                'NODE_OPCALL' => {
+                  'recv' => {
+                    'NODE_INTEGER' => 1
+                  },
+                  'mid' => :/,
+                  'args' => {
+                    'NODE_LIST' => [
+                      {
+                        'NODE_RATIONAL' => 17
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_parse_rational_times_opcall
+    result = Mjollnir.parse('1/17r * 1/17r')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_OPCALL' => {
+              'recv' => {
+                'NODE_OPCALL' => {
+                  'recv' => {
+                    'NODE_INTEGER' => 1
+                  },
+                  'mid' => :/,
+                  'args' => {
+                    'NODE_LIST' => [
+                      'NODE_RATIONAL' => 17
+                    ]
+                  }
+                }
+              },
+              'mid' => :*,
+              'args' => {
+                'NODE_LIST' => [
+                  {
+                    'NODE_INTEGER' => 1
+                  }
+                ]
+              }
+            }
+          },
+          'mid' => :/,
+          'args' => {
+            'NODE_LIST' => [
+              'NODE_RATIONAL' => 17
+            ]
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_parse_rational_div_opcall
+    result = Mjollnir.parse('1/17r / 1/17r')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_OPCALL' => {
+              'recv' => {
+                'NODE_OPCALL' => {
+                  'recv' => {
+                    'NODE_INTEGER' => 1
+                  },
+                  'mid' => :/,
+                  'args' => {
+                    'NODE_LIST' => [
+                      'NODE_RATIONAL' => 17
+                    ]
+                  }
+                }
+              },
+              'mid' => :/,
+              'args' => {
+                'NODE_LIST' => [
+                  {
+                    'NODE_INTEGER' => 1
+                  }
+                ]
+              }
+            }
+          },
+          'mid' => :/,
+          'args' => {
+            'NODE_LIST' => [
+              'NODE_RATIONAL' => 17
+            ]
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_parse_rational_remainder_opcall
+    result = Mjollnir.parse('1/17r % 1/17r')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_OPCALL' => {
+              'recv' => {
+                'NODE_OPCALL' => {
+                  'recv' => {
+                    'NODE_INTEGER' => 1
+                  },
+                  'mid' => :/,
+                  'args' => {
+                    'NODE_LIST' => [
+                      'NODE_RATIONAL' => 17
+                    ]
+                  }
+                }
+              },
+              'mid' => :%,
+              'args' => {
+                'NODE_LIST' => [
+                  {
+                    'NODE_INTEGER' => 1
+                  }
+                ]
+              }
+            }
+          },
+          'mid' => :/,
+          'args' => {
+            'NODE_LIST' => [
+              'NODE_RATIONAL' => 17
+            ]
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_parse_rational_call
+    result = Mjollnir.parse('1/17r.to_i')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_INTEGER' => 1
+          },
+          'mid' => :/,
+          'args' => {
+            'NODE_LIST' => [
+              {
+                'NODE_CALL' => {
+                  'recv' => {
+                    'NODE_RATIONAL' => 17
+                  },
+                  'mid' => :to_i,
+                  'args' => nil
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+
+  def test_parse_rational_call_with_arg
+    result = Mjollnir.parse('1/17r.to_i(10)')
+
+    expected = {
+      'NODE_SCOPE' => {
+        'NODE_OPCALL' => {
+          'recv' => {
+            'NODE_INTEGER' => 1
+          },
+          'mid' => :/,
+          'args' => {
+            'NODE_LIST' => [
+              {
+                'NODE_CALL' => {
+                  'recv' => {
+                    'NODE_RATIONAL' => 17
+                  },
+                  'mid' => :to_i,
+                  'args' => {
+                    'NODE_LIST' => [
+                      {
+                        'NODE_INTEGER' => 10
+                      }
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    assert_equal expected, result
+  end
+end
