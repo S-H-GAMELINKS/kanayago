d456514fa3114a7cc3235a6d4cdfa9633229d124
S-H-GAMELINKS <gamelinks007@gmail.com>
2024-09-01 16:18:41 +0900

    Add class description

--- lib/refine_tree.rb
+++ lib/refine_tree.rb
@@ -3,6 +3,7 @@
 require_relative 'refine_tree/version'
 require_relative 'refine_tree/refine_tree'
 
+# Parse Ruby code with Ruby's Parser(Universal Parser)
 module RefineTree
   def self.parse(source)
     refine_tree_parse(source)
