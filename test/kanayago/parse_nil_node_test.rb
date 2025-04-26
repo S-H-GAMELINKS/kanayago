9b53b94693bea4ce2302e5120f87bed2542f6b9a
S-H-GAMELINKS <gamelinks007@gmail.com>
2025-04-26 22:13:48 +0900

    Support NODE_NIL

---
+++ test/kanayago/parse_nil_node_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+class ParseNilNodeTest < Minitest::Test
+  def test_parse_nil_node
+    result = Kanayago.parse('nil')
+
+    body = result.body
+
+    assert_instance_of(Kanayago::NilNode, body)
+    assert_nil(body.val)
+  end
+end
