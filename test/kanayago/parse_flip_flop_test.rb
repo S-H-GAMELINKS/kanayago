95c7c1e34b9e4270db2f1c4d2e05e8eccb21b492
S-H-GAMELINKS <gamelinks007@gmail.com>
2025-10-02 21:43:31 +0900

    Support NODE_FLIP2 and NODE_FLIP3

---
+++ test/kanayago/parse_flip_flop_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+class ParseFlipFlopTest < Minitest::Test
+  def test_parse_inclusive_flip_flop
+    result = Kanayago.parse(<<~CODE)
+      if (1==1)..(2==2)
+        puts "ok"
+      end
+    CODE
+
+    assert_instance_of(Kanayago::ScopeNode, result)
+    if_node = result.body
+
+    assert_instance_of(Kanayago::IfStatementNode, if_node)
+
+    flip_flop = if_node.cond
+
+    assert_instance_of(Kanayago::FlipFlopNode, flip_flop)
+    refute_nil(flip_flop.beg)
+    refute_nil(flip_flop.end)
+  end
+
+  def test_parse_exclusive_flip_flop
+    result = Kanayago.parse(<<~CODE)
+      if (1==1)...(2==2)
+        puts "ok"
+      end
+    CODE
+
+    assert_instance_of(Kanayago::ScopeNode, result)
+    if_node = result.body
+
+    assert_instance_of(Kanayago::IfStatementNode, if_node)
+
+    flip_flop = if_node.cond
+
+    assert_instance_of(Kanayago::ExclusiveFlipFlopNode, flip_flop)
+    refute_nil(flip_flop.beg)
+    refute_nil(flip_flop.end)
+  end
+end
